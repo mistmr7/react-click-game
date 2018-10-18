e59ebc0c78f9ebf1851ce7df1eb78031eefc3b33
mistmr7 <michael.mistarz@gmail.com>
2018-10-18 13:19:36 -0400

    Updates

---
+++ precache-manifest.4dd4be94756d78c0175b8e5652383ca0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "39ce7167e47432cca7c2",
+    "url": "/react-click-game/static/js/runtime~main.39ce7167.js"
+  },
+  {
+    "revision": "7b1b9e7d50f61cef1161",
+    "url": "/react-click-game/static/js/main.7b1b9e7d.chunk.js"
+  },
+  {
+    "revision": "13a15114ae3ac597de25",
+    "url": "/react-click-game/static/js/1.13a15114.chunk.js"
+  },
+  {
+    "revision": "7b1b9e7d50f61cef1161",
+    "url": "/react-click-game/static/css/main.c4bf1991.chunk.css"
+  },
+  {
+    "revision": "790905a569e67f91053406260aa86ad6",
+    "url": "/react-click-game/index.html"
+  }
+];
