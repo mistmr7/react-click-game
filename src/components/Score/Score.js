dcb6b1b2abaee58ca61c3e6e4f1a2786e01105d7
mistmr7 <michael.mistarz@gmail.com>
2018-10-18 13:13:42 -0400

    commit all changes to files, fixed high score and added welcome message

--- src/components/Score/Score.js
+++ src/components/Score/Score.js
@@ -4,61 +4,74 @@ import Card from '../Card/Card'
 // import coco from './coco.json'
 import marioKart from '../../../src/marioKart.json'
 
+// Function used to shuffle all of the cards
+const shuffle = (array) => {
+  let currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+  
+  return array; 
+}
+
 class Score extends Component {
   state = {
     marioKart,
     score: 0,
     highScore: 0,
-    chosen:[]
+    scores:[],
+    chosen:[],
+    welcomeMessage: "Click on any player to begin. Clicking any character more than once will result in a loss. Good luck!"
   }
 
     
   imageClickedHandler = (event) => {
-    
+    // check to see if the card has been chosen, if so, add 1 to the score
     if (this.state.chosen.indexOf(parseInt(event.target.id)) === -1){
       this.setState( (state) => { return {
         score: state.score + 1    
       }})
-      const shuffle = (array) => {
-        let currentIndex = array.length, temporaryValue, randomIndex;
       
-        // While there remain elements to shuffle...
-        while (0 !== currentIndex) {
-      
-          // Pick a remaining element...
-          randomIndex = Math.floor(Math.random() * currentIndex);
-          currentIndex -= 1;
-      
-          // And swap it with the current element.
-          temporaryValue = array[currentIndex];
-          array[currentIndex] = array[randomIndex];
-          array[randomIndex] = temporaryValue;
-        }
-        
-        return array; 
-      }
+      // push the new target id into the selected array
       this.state.chosen.push(parseInt(event.target.id))
 
-      this.setState(state => { return {
-        highScore: Math.max(state.highScore || state.score), 
-        marioKart: shuffle(marioKart)           
-      }})
+      // push the score to the scores array
+      this.state.scores.push(this.state.score + 1)
+      
+      //check the scores array for the highest score and set it to the high Score
+      this.state.highScore = Math.max.apply(null, this.state.scores)
+        
+        this.setState({
+          highScore: this.state.highScore,
+          scores: this.state.scores,
+          chosen: this.state.chosen, 
+          marioKart: shuffle(marioKart),
+          welcomeMessage: "Nice choice!"           
+        })    
 
     } else {
-      this.setState({
-        score: 0
+      let score = 0;
+      return this.setState({
+        score: score,
+        highScore:this.state.highScore,
+        scores: this.state.scores,
+        chosen:[],
+        welcomeMessage: "Sorry, that item has already been clicked! Play again to see if you can set a new high Score!"
       })
     }
   }
 
-  highScoreChecker = (event) => {
-    console.log('clicked')
-    if (this.state.highScore < this.state.score) {
-      this.setState({
-        highScore: this.state.score
-      })
-    }
-  }
+
 
  
   
@@ -68,10 +81,11 @@ class Score extends Component {
       <nav className="navbar navbar-dark bg-dark">
         <span className="navbar-brand mb-0 h1">React-Clicky-Game</span>
         <ul className="score">
-          <li onChange={this.highScoreChecker} value={this.state.score}>Score: {this.state.score}</li>
+          <li value={this.state.score}>Score: {this.state.score}</li>
           <li>High Score: {this.state.highScore}</li>
         </ul>
       </nav>
+      <p className="text-center">{this.state.welcomeMessage}</p>
       <div className="row justify-content-center">
         {this.state.marioKart.map(character => (
           <Card 
